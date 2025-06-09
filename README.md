113d31c08c643a821639bbbfd7bc68356003fd81
unknown <elderculquig@gmail.com>
2025-06-09 15:00:28 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# test-sonar-node
+
+Test sonar node
+
+### probando un pipeline en drone
---
+++ index.js
@@ -0,0 +1,5 @@
+function saludo(nombre) {
+  return `Hola, ${nombres} esto es otra prueba no funciona! esta si es`;
+}
+
+console.log(saludo("Mundo"));
